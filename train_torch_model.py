ce9a9c06c623bc5ab232f61583abeb7639a7e464
hugo flores <hf01049@georgiasouthern.edu>
2020-08-26 17:38:50 -0400

    trained neural net w mixup

--- train_torch_model.py
+++ train_torch_model.py
@@ -39,31 +39,31 @@ def mixup_data(x, y, alpha=1.0):
     y_a, y_b = y, y[index]
     return mixed_x, y_a, y_b, lam
 
-def mixup_criterion(y_a, y_b, lam):
-    return lambda criterion, pred: lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+def mixup_criterion(pred, y_a, y_b, lam, criterion):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
 
 class NN(pl.LightningModule):
 
-    def __init__(self, 
-                num_epochs, 
-                learning_rate=1e-3,
-                random_seed=42,
-                train_val_test_split=np.array([0.6, 0.2, 0.2]),
-                batch_size=128, 
-                dropout=0.1):
+    def __init__(self, hparams):
         super().__init__()
         self.fc1 = nn.Linear(6144, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 19)
 
-        self.seed = random_seed
-        self.num_epochs = num_epochs
-        self.splits = train_val_test_split
-        self.learning_rate = learning_rate
-        self.batch_size = batch_size
-        self.dropout_p = dropout
+        self.hparams = hparams
+        self.splits = np.array([0.6, 0.2, 0.2])
+        self.learning_rate = hparams.learning_rate
+        self.batch_size = hparams.batch_size
+        self.dropout_p = hparams.dropout
 
-        pl.seed_everything(random_seed)
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--num_workers',    default=4, type=int)
+        parser.add_argument('--learning_rate',  default=1e-3)
+        parser.add_argument('--batch_size',     default=128)
+        parser.add_argument('--dropout',        default=0.1)
+        return parser
 
     def forward(self, x):
 
@@ -87,21 +87,27 @@ class NN(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         X, y = batch['embedding'], batch['one_hot']
+        
+        mixup = True
+        # mixup would go here
+        if mixup:
+            mixed_x, y_a, y_b, lam = mixup_data(X, y, alpha=1.0)
+            X = mixed_x
+
         # for nll loss
         y = torch.argmax(y, dim=1, keepdim=False)
+        y_a = torch.argmax(y_a, dim=1, keepdim=False)
+        y_b = torch.argmax(y_b, dim=1, keepdim=False)
 
         X = self.preprocess(X)
 
-        # mixup would go here
-
-        logits = self.forward(X)
-        loss = self.cross_entropy_loss(logits, y)
+        logits = self.forward(X) 
+        loss = self.cross_entropy_loss(logits, y) if not mixup else mixup_criterion(logits, y_a, y_b, lam, criterion=self.cross_entropy_loss)
 
         result = pl.TrainResult(minimize=loss)
-        result.log('train_loss', loss, prog_bar=True)
+        result.log('train_loss', loss,  on_step=True)
 
         return result
-
 
     def validation_step(self, batch, batch_idx):
         X, y = batch['embedding'], batch['one_hot']
@@ -113,10 +119,30 @@ class NN(pl.LightningModule):
 
         yhat = torch.argmax(logits, dim=1, keepdim=False)
         accuracy = pl.metrics.functional.accuracy(yhat, y)
+        f1 = pl.metrics.functional.f1_score(yhat, y)
 
-        result = pl.EvalResult(checkpoint_on=loss)
-        result.log('val_accuracy', accuracy, on_epoch=True, on_step=False, prog_bar=True)
-        result.log('val_loss', loss, prog_bar=True)
+        result = pl.EvalResult(checkpoint_on=loss, early_stop_on=loss)
+        result.log('val_accuracy', accuracy, on_epoch=True, on_step=False, )
+        result.log('val_f1', f1, on_epoch=True, on_step=False, )
+        result.log('val_loss',  loss)
+        return result
+    
+    def test_step(self, batch, batch_idx):
+        X, y = batch['embedding'], batch['one_hot']
+        y = torch.argmax(y, dim=1, keepdim=False)
+
+        X = self.preprocess(X)
+        logits = self.forward(X)
+        loss = self.cross_entropy_loss(logits, y)
+
+        yhat = torch.argmax(logits, dim=1, keepdim=False)
+        accuracy = pl.metrics.functional.accuracy(yhat, y)
+        f1 = pl.metrics.functional.f1_score(yhat, y)
+
+        result = pl.EvalResult()
+        result.log('test_accuracy', accuracy, on_epoch=True, on_step=False)
+        result.log('test_f1', f1, on_epoch=True, on_step=False, )
+        result.log('test_loss', loss, on_step=True, on_epoch=False)
         return result
 
     def prepare_data(self):
@@ -125,7 +151,6 @@ class NN(pl.LightningModule):
                      'silence_split', 'openl3-mel256-6144-music'), 
             classes='no_percussion')
 
-        print(len(self.dataset))
         lengths = (len(self.dataset)*self.splits).astype(int)
         # oof. hackiest thing ever.
         while sum(lengths) < len(self.dataset):
@@ -136,82 +161,68 @@ class NN(pl.LightningModule):
                         lengths=lengths)
 
     def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=self.batch_size, collate_fn=collate_embeddings)
+        return DataLoader(self.train_data, batch_size=self.batch_size, 
+            collate_fn=collate_embeddings, shuffle=True, num_workers=self.hparams.num_workers)
 
     def val_dataloader(self):  
-        return DataLoader(self.val_data, batch_size=self.batch_size, collate_fn=collate_embeddings)
+        return DataLoader(self.val_data, batch_size=self.batch_size, 
+            collate_fn=collate_embeddings, shuffle=False, num_workers=self.hparams.num_workers)
 
     def test_dataloader(self):
-        return DataLoader(self.test_data, batch_size=self.batch_size, collate_fn=collate_embeddings)
+        return DataLoader(self.test_data, batch_size=self.batch_size, 
+            collate_fn=collate_embeddings, shuffle=False, num_workers=self.hparams.num_workers)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
         return optimizer
 
-# ------------------
-# | HYPERPARAMETERS |
-# ------------------
+def train(hparams):
 
-# MODEL ARGS
-num_epochs = 500
-learning_rate = 1e-3
-random_seed = 42
-train_split = 0.6
-val_split = 0.2
-test_split = 0.2
-batch_size = 128
+    pl.seed_everything(hparams.random_seed)
 
-def train(
-            # MODEL ARGS
-            num_epochs=num_epochs, 
-            learning_rate=learning_rate,
-            random_seed=random_seed,
-            train_val_test_split=np.array([0.6, 0.2, 0.2]),
-            batch_size=batch_size, 
- 
-            verbose=True):
-
-    model = NN(
-        num_epochs=num_epochs, 
-        learning_rate=learning_rate,
-        random_seed=random_seed,
-        train_val_test_split=train_val_test_split,
-        batch_size=batch_size
-    )
-
-    early_stopping = pl.callbacks.EarlyStopping(
-        monitor='val_loss', 
-        min_delta='1e-5', 
-        patience=5, 
-        verbose=verbose,
-    )
+    model = NN(hparams)
 
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        filepath=os.path.join(os.getcwd(), 'checkpoints'), 
+        filepath=os.path.join(os.getcwd(), 'logs', hparams.name, 'checkpoints', '{epoch}-{val_loss:.2f}'), 
         monitor='val_loss', 
-        verbose=verbose
+        verbose=hparams.verbose, 
+        save_top_k=1, 
     )
 
-    trainer = pl.Trainer(
-        default_root_dir=os.path.join(os.getcwd(), 'logs'),
-        early_stop_callback=early_stopping, 
-        checkpoint_callback=checkpoint_callback, 
-        fast_dev_run=False)
+    logger = pl.loggers.TensorBoardLogger(
+        save_dir='./logs', 
+        name=hparams.name, 
+        version=0
+    )
 
-    # if os.path.exists(checkpoint_dir):
-    #     print(f'loading checkpoint: {checkpoint_dir}')
-    #     trainer =  pl.Trainer(resume_from_checkpoint=checkpoint_dir)
+    trainer = pl.Trainer.from_argparse_args(
+        args=hparams,
+        default_root_dir=os.path.join(os.getcwd(), 'logs'),
+        early_stop_callback=True, 
+        checkpoint_callback=checkpoint_callback, 
+        logger=logger,
+    )
 
     trainer.fit(model)
+    trainer.test()
+
 
 if __name__ == "__main__":
-    train(
-        # MODEL ARGS
-        num_epochs=num_epochs, 
-        learning_rate=learning_rate,
-        random_seed=random_seed,
-        train_val_test_split=np.array([train_split, val_split, test_split]),
-        batch_size=batch_size, 
+    import argparse
+    parser = argparse.ArgumentParser()
 
-        verbose=True
-    )
+    # ------------------
+    # | HYPERPARAMETERS |
+    # ------------------
+
+    parser = NN.add_model_specific_args(parser)
+    parser = pl.Trainer.add_argparse_args(parser)
+
+    parser.add_argument('--num_epochs',     default=70)
+    parser.add_argument('--name',           default='exp', type=str)
+    parser.add_argument('--random_seed',    default=420)
+    parser.add_argument('--verbose',        default=True, type=bool)
+
+    hparams = parser.parse_args()
+
+    train(hparams)
